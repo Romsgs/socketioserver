f536d4d6f4ce7f1d8adabcacb921ed09dcc7ce29
Romsgs <rsalobrena@gmail.com>
2023-11-02 07:30:35 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const http = require('http')
+const {Server} = require('socket.io')
+const cors = require('cors')
+app.use(cors())
+
+const Server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
