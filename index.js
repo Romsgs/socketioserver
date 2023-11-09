c9962344abb8b0cf6fb226a07af7b6a952f5ae67
Romsgs <rsalobrena@gmail.com>
2023-11-09 00:03:23 -0300

    front

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 // Create an Express app
 const app = express();
 const server = http.createServer(app);
 
 // Add middleware
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Create a Socket.io server
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 // Socket.io event handling
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`User connected ${socket.id}`);
 
-   socket.on('I_Know', (data) => {
+  socket.on("I_Know", (data) => {
     // Implement message validation and sanitization here if needed
-    socket.emit("whoKnows", data.nome);
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours().toString().padStart(2, "0");
+    const currentMinute = currentDate.getMinutes().toString().padStart(2, "0");
+    const currentSecond = currentDate.getSeconds().toString().padStart(2, "0");
+    const formattedTime = `${currentHour}:${currentMinute}:${currentSecond}`;
+    socket.emit("whoKnows", { nome: data.nome, time: formattedTime });
     socket.broadcast.emit("whoKnows", data.nome);
-  });  
+  });
 
-  socket.on('reset', () => {
+  socket.on("reset", () => {
     socket.emit("reseted");
     socket.broadcast.emit("reseted");
   });
 
   // Handle disconnect event
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`User disconnected ${socket.id}`);
   });
 });
@@ -43,7 +48,7 @@ io.on('connection', (socket) => {
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ error: 'Server error' });
+  res.status(500).json({ error: "Server error" });
 });
 
 // Start the server
